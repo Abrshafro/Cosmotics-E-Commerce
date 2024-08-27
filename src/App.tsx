59288806641866864cc4706156d745f62e360add
Abrshafro <abrhammgetahun@gmail.com>
2024-08-27 14:11:38 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import{Route,BrowserRouter as Router, Routes} from "react-router-dom";
+//import { Router } from "lucide-react";
+import Sidebar from "./components/Sidebar";
+import MainContent from "./components/MainContent";
+import ProductPage from "./components/ProductPage";
+
+
+export default function App() {
+  return(
+     <Router>
+    <div className="flex h-screen">
+      <Sidebar/>
+      <div className="rounded w-full flex justify-center flex-wrap">
+        <Routes>
+          <Route path="/" element={<MainContent/>}/>
+          <Route path="/product/:id" element={<ProductPage/>}/>
+          
+        </Routes>
+      </div>
+
+    </div>
+  </Router>
+  );
+}
